c1d0578ac55f101cc36ad54f6672332475d0ef3c
Frankie Roberto <frankie@frankieroberto.com>
2016-11-03 14:16:59 +0000

    work-in-progress interface for transcribing index

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -18,6 +18,17 @@ class RecordsController < ApplicationController
     @images = @record.images.order(:sequence)
   end
 
+  def index_pages
+    @record = Record.find(params[:record_id])
+    @images = @record.images.where(page_type: 'Index').order(:sequence)
+  end
+
+  def index_page
+    @record = Record.find(params[:record_id])
+    @image = @record.images.find(params[:id])
+    @patients = Patient.order(:name)
+  end
+
   def update_pages
 
     record = Record.find(params[:record_id])
@@ -47,6 +58,20 @@ class RecordsController < ApplicationController
     record.update_pages_with_type_count!
 
     redirect_to record_path(record)
+  end
+
+
+  def update_index_page
+    record = Record.find(params[:record_id])
+    image = record.images.find(params[:id])
+
+    next_image = record.images.find_by(sequence: image.sequence + 1)
+
+    if next_image.page_type == 'Index'
+      redirect_to record_index_page_path(record, next_image.id)
+    else
+      redirect_to record_index_path(record)
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,13 @@ Rails.application.routes.draw do
   resources :records, only: [:show, :index] do
 
     resource :pages, only: [:show], controller: :records, action: :pages
-    resource :pages, only: [:show, :update], controller: :records, action: :update_pages
+    resource :pages, only: [:update], controller: :records, action: :update_pages
 
+
+    resource :index, only: [:show], controller: :records, action: :index_pages, as: 'index' do
+      resources :index_page, only: [:show], controller: :records, action: :index_page, path: '', as: 'page'
+      resources :index_page, only: [:update], controller: :records, action: :update_index_page, path: '', as: 'page'
+    end
   end
 
   root to: "home#show"
